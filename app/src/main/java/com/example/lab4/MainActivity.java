4bb0c6625187e869aff74c2ede7fb7d0493a6d24
Harry <hpreston2909@gmail.com>
2021-01-30 13:33:27 +0000

    Added javadoc and generated.

--- app/src/main/java/com/example/lab4/MainActivity.java
+++ app/src/main/java/com/example/lab4/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.lab4;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,12 +7,17 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
 
+    // returns the simple name of the underlying class. defined to keep a log
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
+    // message string
     public static final String EXTRA_MESSAGE = "com.example.lab4.MainActivity.extra.MESSAGE";
 
+    // defining the rest of the variables
     private EditText mMessageEditText;
 
     public static final int TEXT_REQUEST = 1;
@@ -22,6 +25,11 @@ public class MainActivity extends AppCompatActivity {
     private TextView mReplyHeadTextView;
     private TextView mReplyTextView;
 
+    /**
+     * OnCreate method is called when the activity starts.
+     *
+     * @param savedInstanceState is used to save the most recently supplied data to the app.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +39,13 @@ public class MainActivity extends AppCompatActivity {
         mReplyTextView = findViewById(R.id.text_message_reply);
     }
 
+    /**
+     * Method to launch second activity on button press. Logcat message is set.
+     * Using intent to bundle the message from main activity "EXTRA_MESSAGE".
+     * Start activityforresult is used because we expect a result back.
+     *
+     * @param view used for drawing and handling events.
+     */
     public void launchSecondActivity(View view) {
         Log.d(LOG_TAG, "Button clicked!");
         Intent intent = new Intent(this, SecondActivity.class);
@@ -39,12 +54,23 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(intent, TEXT_REQUEST);
     }
 
+    /**
+     * This method is used if result is registered. (Data is sent back). Check that the request
+     * code is equal to the one defined.
+     * Check that the result code also equals the same.
+     * Then, using the intent from the result, get the message sent and set it into the
+     * main activity's textview.
+     *
+     * @param requestCode used to check it aligns with the variable defined in mainactivity.
+     * @param resultCode  used to check it aligns with the variable defined in mainactivity.
+     * @param data        the intent data sent from secondactivity.
+     */
     @Override
     public void onActivityResult(int requestCode,
                                  int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == TEXT_REQUEST){
-            if (resultCode == RESULT_OK){
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
                 String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
                 mReplyHeadTextView.setVisibility(View.VISIBLE);
                 mReplyTextView.setText(reply);
--- app/src/main/java/com/example/lab4/SecondActivity.java
+++ app/src/main/java/com/example/lab4/SecondActivity.java
@@ -1,19 +1,27 @@
 package com.example.lab4;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class SecondActivity extends AppCompatActivity {
 
+    // Reply message
     public static final String EXTRA_REPLY = "com.example.lab4.MainActivity.extra.REPLY";
 
+    // Edit text field
     private EditText mReply;
 
+    /**
+     * OnCreate method is called when the activity starts.
+     * Get Intent from MainActivity and then put the message from reply back into it.
+     *
+     * @param savedInstanceState is used to save the most recently supplied data to the app.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +33,12 @@ public class SecondActivity extends AppCompatActivity {
         textView.setText(message);
     }
 
+    /**
+     * Get reply text from the edit text field.
+     * Define new intent, put the reply back as the "EXTRA_REPLY" message.
+     *
+     * @param view used for drawing and event handling.
+     */
     public void returnReply(View view) {
         String reply = mReply.getText().toString();
         Intent replyIntent = new Intent();
